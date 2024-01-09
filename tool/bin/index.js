2b541e7f2fb3aeb711c8a14a78678e78f15f80ad
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-01-10 00:32:10 +0100

    Refactor code to use ES6 imports and add package.json configuration check

--- tool/bin/index.js
+++ tool/bin/index.js
@@ -1,14 +1,39 @@
 #!/usr/bin/env node
-const arg = require('arg');
-const chalk = require('chalk');
+import arg from 'arg'
+import chalk from 'chalk'
+import { readPackageUp } from 'read-package-up'
+
+async function main() {
+  const result = await readPackageUp({ cwd: 'package.json' })
+  return result
+}
 
 try {
   const args = arg({
     '--start': Boolean,
-    '--build': Boolean,
+    '--build': Boolean
   });
 
   if (args['--start']) {
+    // Agora esperamos que a promessa seja resolvida antes de continuar.
+    const pkgPath = await main();
+
+    if (!pkgPath) {
+      throw new Error('Could not find package.json');
+    }
+
+    // Atribua packageJson a pkg para acessar seus campos.
+    const pkg = pkgPath.packageJson;
+
+    if (pkg.tool) {
+      console.log('Found configuration', pkg.tool);
+      // TODO: do something with configuration
+    } else {
+      console.log(chalk.yellow('Could not find configuration, using default'));
+      // TODO: get default configuration
+    }
+
+    console.log(chalk.bgMagentaBright(`Starting ${pkg.name} v${pkg.version}`));
     console.log(chalk.bgCyanBright('starting the app'));
   }
 } catch (e) {
