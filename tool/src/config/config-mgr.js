70ae99cc9f01a0bc23d52d0dc1b020cd051ec8ca
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-01-10 01:30:49 +0100

    Add dependencies and configuration loader

--- tool/src/config/config-mgr.js
+++ tool/src/config/config-mgr.js
@@ -1,6 +1,7 @@
 import chalk from 'chalk'
-
+import { cosmiconfigSync, cosmiconfig } from 'cosmiconfig'
 // const pkgUp = require('pkg-up');
+
 import { readPackageUp } from 'read-package-up'
 
 export async function getPkgFile() {
@@ -9,18 +10,25 @@ export async function getPkgFile() {
 }
 
 export const getConfig = async () => {
+  const configLoader = cosmiconfigSync('tool')
+  const result = configLoader.search()
+
   const pkgPath = await getPkgFile()
   const pkg = pkgPath.packageJson
   if (!pkgPath) {
     throw new Error('Could not find package.json')
   }
 
-  if (pkg.tool) {
-    console.log('Found configuration', pkg.tool)
+  if (pkg.name) {
     console.log(chalk.bgMagentaBright(`Starting ${pkg.name} v${pkg.version}`))
+  }
+
+  if (result ) {
+    console.log('Found configuration', result.config)
     console.log(chalk.bgCyanBright('starting the app'))
-    return pkg.tool
-  } else {
+    return result.config
+  }
+  if (!result ) {
     console.log(chalk.yellow('Could not find configuration, using default'))
     return { port: 1234 }
   }
